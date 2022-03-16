a05be5505c5ad52c990fc220387a28dbaa3293d9
huhn511 <sehe89@gmail.com>
2022-03-16 23:38:23 +0100

    add transparency

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -22,7 +22,7 @@ const initialCells = Array.from({ length: 42 * 42 }, () => offCell)
 export const Pixelbots = (any) => {
   const [cells, setCells] = useState(initialCells)
   const [loading, setLoading] = useState(false)
-  const [currentColor, setCurrentColor] = useState('#56BC58')
+  const [currentColor, setCurrentColor] = useState('#0FD698')
   const colorSwatch = useMemo(
     () => [
       ...Array.from(
@@ -45,22 +45,92 @@ export const Pixelbots = (any) => {
     for (var i = 0; i < elements.length; i++) {
       //@ts-ignore
       elements[i].style.border = 'none'
+      // elements[i].style.backgroundColor = '#0FD698'
     }
     domtoimage
-    .toPng(node)
-    .then(function (dataUrl) {
-      var img = new Image()
-      img.src = dataUrl
-      console.log('image', img)
-      myCollection.appendChild(img)
-      for (var i = 0; i < elements.length; i++) {
+      .toPng(node)
+      .then(function (dataUrl) {
+        // var img = new Image()
+        var canvas = null
+        var ctx = null
+
+        var img = new Image()
+        // img.setAttribute('src', dataUrl)
+        img.src = dataUrl
+        img.onload = function () {
+          copyImageToCanvas(img)
+          console.log('image', img)
+          var dataURL = canvas.toDataURL('image/png')
+          var img2 = new Image()
+          img2.src = dataURL
+          myCollection.appendChild(img2)
+        }
+
+      
+        for (var i = 0; i < elements.length; i++) {
           //@ts-ignore
           elements[i].style.border = '.5px solid black'
         }
         setLoading(false)
+
+        function copyImageToCanvas(aImg) {
+          canvas = document.createElement('canvas')
+
+          var w =
+            typeof aImg.naturalWidth == 'undefined'
+              ? aImg.width
+              : aImg.naturalWidth
+          var h =
+            typeof aImg.naturalHeight == 'undefined'
+              ? aImg.height
+              : aImg.naturalHeight
+
+          canvas.id = 'img' + 1
+          canvas.width = w
+          canvas.height = h
+
+          // $this.replaceWith(canvas)
+
+          ctx = canvas.getContext('2d')
+          ctx.clearRect(0, 0, w, h)
+          ctx.drawImage(aImg, 0, 0)
+
+          makeTransparent(aImg)
+         
+        }
+
+        function makeTransparent(aImg) {
+          var w =
+            typeof aImg.naturalWidth == 'undefined'
+              ? aImg.width
+              : aImg.naturalWidth
+          var h =
+            typeof aImg.naturalHeight == 'undefined'
+              ? aImg.height
+              : aImg.naturalHeight
+          var imageData = ctx.getImageData(0, 0, w, h)
+          console.log('imageData.widt', imageData.width)
+          console.log('imageData.height', imageData.height)
+          for (var x = 0; x < imageData.width; x++)
+            for (var y = 0; y < imageData.height; y++) {
+              var offset = (y * imageData.width + x) * 4
+              var r = imageData.data[offset]
+              var g = imageData.data[offset + 1]
+              var b = imageData.data[offset + 2]
+
+              //if it is pure white, change its alpha to 0
+              if (r == 255 && g == 255 && b == 255)
+                imageData.data[offset + 3] = 0
+              imageData.data[offset] = 0
+            }
+
+          ctx.putImageData(imageData, 0, 0)
+          return img
+        }
       })
       .catch(function (error) {
         console.error('oops, something went wrong!', error)
+        setLoading(false)
       })
   }
 
@@ -79,7 +149,6 @@ export const Pixelbots = (any) => {
     var inputElem = document.getElementById('input')
     //@ts-ignore
     setCells(JSON.parse(inputElem.value))
-    
   }
 
   return (
@@ -136,14 +205,14 @@ export const Pixelbots = (any) => {
                 Save
               </Button>
               <Button disabled={loading} onClick={() => save_as_json()}>
-              save_as_json
+                save_as_json
               </Button>
               <br />
               <div id='my-collection'></div>
-              <a id="downloadAnchorElem" style={{display:'none'}}></a>
-              <input id="input" />
+              <a id='downloadAnchorElem' style={{ display: 'none' }}></a>
+              <input id='input' />
               <Button disabled={loading} onClick={() => apply()}>
-              apply
+                apply
               </Button>
               {/* {chatString} */}
             </p>
